08960fc0eb6acb3c9c57304b36fec881d5bfdcb6
Lucas Vicentim <lucasvicentim0301@gmail.com>
2021-06-04 13:58:53 +0200

    CONTROLLER: InputHandler

--- src/de/tum/ase/eist/usercontrol/KeyboardInput.java
+++ src/de/tum/ase/eist/usercontrol/KeyboardInput.java
@@ -1,4 +1,25 @@
 package de.tum.ase.eist.usercontrol;
 
+import java.awt.event.KeyEvent; 
+import de.tum.ase.eist.model.Player;
+import de.tum.ase.eist.model.Asteroids;
+
 public class KeyboardInput {
+
+    private Player player = null;
+    private Asteroids asteroids = null;
+
+    public KeyboardInput (Asteroids asteroids, Player player) {
+        this.player = player;
+        this.asteroids = asteroids;
+    }
+
+    public void keyPressing (KeyEvent e) {
+        //pass
+    }
+
+    public void keyReleasing (KeyEvent e) {
+        //pass
+    }
+
 }
--- src/de/tum/ase/eist/usercontrol/MouseClicking.java
+++
@@ -1,4 +0,0 @@
-package de.tum.ase.eist.usercontrol;
-
-public class MouseClicking {
-}
